7a717485a7cca9909f6969f234b3bc30070cc51b
allisontimossi <allison.timossi@hotmail.it>
2024-06-19 15:49:30 +0200

    metodi di console

--- seconda_lezione/esercitazione/applicazione console/Program.cs
+++ seconda_lezione/esercitazione/applicazione console/Program.cs
@@ -1,7 +1,29 @@
-﻿Console.WriteLine("Hello, Allison!");
+﻿/*
+Console.WriteLine("Hello, Allison!");
+Console.Write("Inserisci il tuo nome: ");
+Console.WriteLine("Allie");
+
+Console.Write("Inserisci il tuo nome: \n");
+Console.WriteLine("Allie");
+
+Console.Write("Inserisci il tuo nome: \t");
+Console.WriteLine("Allie"); 
+*/
 
 // questo è un commento
 
 /*
-Console.WriteLine("Hello, World!");
-/*
+Console.WriteLine("Hello, Allison!");
+*/
+
+
+// Console.WriteLine("Premi un tasto per terminare...");
+// Console.ReadKey();
+
+string nome = Console.ReadLine()!;
+Console.WriteLine(nome);
+
+Console.WriteLine($"Hello, {nome}!");
+
+string cognome = "Timossi"!;
+Console.WriteLine($"Hello, {nome} {cognome}!");

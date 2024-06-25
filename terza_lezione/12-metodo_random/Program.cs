d2a16a76dff6d8e8794180769b0e5c04c1f87bea
allisontimossi <allison.timossi@hotmail.it>
2024-06-25 12:30:41 +0200

    Valore random

---
+++ terza_lezione/12-metodo_random/Program.cs
@@ -0,0 +1,8 @@
+﻿//utilizzo di random
+
+//dichiaro oggetto random
+//intervallo semiaperto, aggiungere 1
+Random random = new Random(); //new è un costruttore, ma serve il volante
+int numero = random.Next(-10,10); //il numero estratto è compreso tra .. e .. (è il volante)
+
+Console.WriteLine($"Il numero casuale è {numero}");

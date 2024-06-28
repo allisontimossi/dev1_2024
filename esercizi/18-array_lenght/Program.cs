e133707b59913285c44472a900833553b3c7c4c9
allisontimossi <allison.timossi@hotmail.it>
2024-06-28 10:38:08 +0200

    array_length

---
+++ esercizi/18-array_lenght/Program.cs
@@ -0,0 +1,10 @@
+﻿string [] nomi = new string [3];
+
+
+nomi[0] = "Mario";
+nomi[1] = "Luigi";
+nomi[2] = "Bowser";
+
+Console.WriteLine($"Ciao {nomi[0]}, {nomi [1]}, {nomi[2]}");
+
+Console.WriteLine($"Il numero di elementi è {nomi.Length}");

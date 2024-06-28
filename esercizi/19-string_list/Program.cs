0588cd2366a1bbb3dd3d84d82bfa20b7a6989f87
allisontimossi <allison.timossi@hotmail.it>
2024-06-28 10:53:19 +0200

    list

---
+++ esercizi/19-string_list/Program.cs
@@ -0,0 +1,6 @@
+﻿List<string> nomi = new List<string>(); //NON DOBBIAMO PREDEFINIRE IL NUMERO DI ELEMENTI
+nomi.Add("Mario");
+nomi.Add ("Luigi");
+nomi.Add ("Bowser");
+nomi.Add ("Ginevra");
+Console.WriteLine($"Ciao {nomi[3]}, {nomi[0]}, {nomi [2]}, {nomi[1]}");

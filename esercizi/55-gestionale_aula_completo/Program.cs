11f297a62cc0a0c94e9e5d854d4c08ba48e25e7f
allisontimossi <allison.timossi@hotmail.it>
2024-07-16 15:55:19 +0200

    gestionale aggiornamento

--- esercizi/55-gestionale_aula_completo/Program.cs
+++ esercizi/55-gestionale_aula_completo/Program.cs
@@ -20,33 +20,86 @@ bool primasquadra = true;
 
 do
 {
-
     scelta = AnsiConsole.Prompt(
         new SelectionPrompt<string>()
             .PageSize(10)
             .MoreChoicesText("[grey](Move up and down to reveal more choice)[/]")
             .AddChoices(new[] {
-                "1. Inserisci partecipante/i","2. Visualizza partecipanti","3. Cerca un partecipante","4. Modifica il nome di un partecipante","5. Crea squadre", "6. Esci dal programma"
+                "1. Edita partecipante/i","2. Visualizza partecipanti","3. Cerca un partecipante","4. Crea squadre", "5. Esci dal programma"
                 })); 
     switch (scelta)
         {
-            case "1. Inserisci partecipante/i":
-                Console.Write("Inserisci il nome del partecipante: ");
-                partecipante = (Console.ReadLine());
-                aula.Add (partecipante);
-
-
-
-                if (File.ReadAllLines(path).Contains(partecipante))
+            case "1. Edita partecipante/i":
+                var edita = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .PageSize(10)
+                    .MoreChoicesText("[grey](Move up and down to reveal more choice)[/]")
+                    .AddChoices(new[] {
+                        "1. Inserisci partecipante/i","2. Modifica il nome di un partecipante","3. Elimina"
+                }));
+                switch (edita)
                 {
-                    Console.WriteLine ("Nome già presente");
-                }
+                case "1. Inserisci il nome del partecipante: ":
+                    Console.Write("Inserisci il nome del partecipante: ");
+                    partecipante = (Console.ReadLine());
+                    aula.Add (partecipante);
+
+                    if (File.ReadAllLines(path).Contains(partecipante))
+                    {
+                        Console.Clear();
+                        Console.WriteLine ("Nome già presente");
+                    }
+                    else
+                    {
+                        File.AppendAllText(path, $"{partecipante}\n");
+                        Console.Clear();
+                        Console.WriteLine ("Il nome è stato aggiunto");
+                    }
+                break;
+                case "2. Modifica il nome di un partecipante":
+                Console.Write("Digita il nome di un partecipante da modificare: ");
+                nome = Console.ReadLine();
+
+                if (aula.Contains(nome)) //per capire se partecipante è presente o no
+                    {
+                        string nuovoNome = Console.ReadLine();
+                        Console.Write("Digita il nome modificato: ");
+                        aula[aula.IndexOf(nome)] = nuovoNome;
+                        Console.WriteLine("Il nome del partecipante è stato modificato.");
+                        Console.WriteLine("Premi un tasto per continuare..");
+                        Console.ReadKey(); 
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Il partecipante non è presente nella lista.");
+                        Console.WriteLine("Premi un tasto per continuare..");
+                        Console.ReadKey(); 
+                        Console.Clear();
+                    }
+                break;
+                case "3. Elimina":
+                var eliminazione = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+            
+                    .PageSize(aula.Count)
+                    .MoreChoicesText("[grey](Move up and down to reveal more choice)[/]")
+                    .AddChoices(aula));
+        
+                if (aula.Contains(eliminazione))
+                    {
+                        aula.Remove(eliminazione);
+                        Console.WriteLine("Il partecipante è stato eliminato dalla lista");
+
+                    }
                 else
-                {
-                    File.AppendAllText(path, $"{partecipante}\n");
-                    Console.WriteLine ("Il nome è stato aggiunto");
-                }          
-            break;
+                    {
+                        Console.WriteLine("Il partecipante non è presente nella lista");
+                    }
+                    break;
+            
+                }
+            break;    
             case "2. Visualizza partecipanti":
                 Console.WriteLine($"Elenco partecipanti: (totale: {aula.Count})");
                     foreach (string line in aula)
@@ -135,60 +188,47 @@ do
                     }
                 }
             break;
-            case "4. Modifica il nome di un partecipante":
-                Console.Write("Digita il nome di un partecipante da modificare: ");
-                nome = Console.ReadLine();
-
-                if (aula.Contains(nome)) //per capire se partecipante è presente o no
-                    {
-                        Console.Write("Nuovo nome: ");
-                        string nuovoNome = Console.ReadLine();
-                        int indice = aula.IndexOf (nome);
-                        aula [indice] = nuovoNome;
-                        Console.WriteLine("Il nome del partecipante è stato modificato.");
-                        Console.WriteLine("Premi un tasto per continuare..");
-                        Console.ReadKey(); 
-                        Console.Clear();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Il partecipante non è presente nella lista.");
-                        Console.WriteLine("Premi un tasto per continuare..");
-                        Console.ReadKey(); 
-                        Console.Clear();
-                    }
-            break;
-            case "5. Crea squadre":
+            case "4. Crea squadre":
                 Console.WriteLine("Ecco le due squadre sorteggiate:");
-                while (aula.Count > 0)
+
+            while (aula.Count > 0)
                 {
-                    int estrazione = random.Next(aula.Count);
-                    if (primasquadra)
-                    {
-                        squadra1.Add(aula[estrazione]);
-                        primasquadra = false;
-                    }
-                    else
-                    {
-                        squadra2.Add(aula[estrazione]);
-                        primasquadra = true;
-                    }
-                    aula.RemoveAt(estrazione);
+                int estrazione = random.Next(aula.Count);
+                if (primasquadra)
+                {
+                    squadra1.Add (aula[estrazione]);
+                    primasquadra = false;
+                }
+                else
+                {
+                    squadra2.Add (aula[estrazione]);
+                    primasquadra = true;
+                }
+                aula.RemoveAt (estrazione);  
                 }
 
-                var table = new Table();
-                    table.AddColumn("Squadra 1");
-                    table.AddColumn("Squadra 2");
-                for (int i = 0; i < aula.Count; i++)
-                    {
-                        table.AddRow(squadra1[i], squadra2[i]);
-                    }
-                AnsiConsole.Write(table);
+            var table = new Table();
+            table.AddColumn("Squadra 1");
+            
+            foreach (string nom in squadra1)
+            {
+                table.AddRow(nom);
+            }
+            AnsiConsole.Write(table);
 
-            break;
-            case "6. Esci dal programma":
+            var table2 = new Table();
+            table2.AddColumn("Squadra 2");
+            foreach (string nom in squadra2)
+            {
+                table2.AddRow(nom);
+            }
+            AnsiConsole.Write(table2);
+
+            break; 
+
+            case "5. Esci dal programma":
                 Console.WriteLine("Arrivederci!");
             break;
         }
     }
-while (scelta != "6. Esci dal programma");
+while (scelta != "5. Esci dal programma");
---
+++ esercizi/57-gestionale_funzioni/Program.cs
@@ -0,0 +1,230 @@
+﻿﻿﻿// versione con console clear in modo da mantenere il menu nella stessa posizione dopo ogni scelta 
+using Spectre.Console;
+
+class Program
+{
+    static List<string> partecipanti = new List<string>();
+    const string filePath = "partecipanti.txt";
+
+    static void Main()
+    {
+        CaricaPartecipanti();
+        int scelta;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("1. Inserisci partecipante");
+            Console.WriteLine("2. Visualizza partecipanti");
+            Console.WriteLine("3. Ordina partecipanti");
+            Console.WriteLine("4. Cerca partecipante");
+            Console.WriteLine("5. Elimina partecipante");
+            Console.WriteLine("6. Modifica partecipante");
+            Console.WriteLine("7. Numero partecipanti");
+            Console.WriteLine("8. Dividi partecipanti in 2 squadre");
+            Console.WriteLine("9. Dividi partecipanti in 2 squadre con random");
+            Console.WriteLine("10. Esci");
+            Console.Write("Scelta: ");
+            scelta = Convert.ToInt32(Console.ReadLine());
+            switch (scelta)
+            {
+                case 1:
+                    InserisciPartecipante();
+                    break;
+                case 2:
+                    VisualizzaPartecipanti();
+                    break;
+                case 3:
+                    OrdinaPartecipanti();
+                    break;
+                case 4:
+                    CercaPartecipante();
+                    break;
+                case 5:
+                    EliminaPartecipante();
+                    break;
+                case 6:
+                    ModificaPartecipante();
+                    break;
+                case 7:
+                    NumeroPartecipanti();
+                    break;
+                case 8:
+                    DividiPartecipantiInSquadre();
+                    break;
+                case 9:
+                    DividiPartecipantiInSquadreRandom();
+                    break;
+                case 10:
+                    Console.WriteLine("Arrivederci!");
+                    break;
+                default:
+                    Console.WriteLine("Scelta non valida");
+                    break;
+            }
+            if (scelta != 10)
+            {
+                Console.WriteLine("Premi un tasto per continuare...");
+                Console.ReadKey();
+            }
+        } while (scelta != 10);
+
+    }
+
+    static void CaricaPartecipanti()
+    {
+        if (File.Exists(filePath))
+        {
+            partecipanti = new List<string>(File.ReadAllLines(filePath));
+        }
+    }
+
+    static void StampaMessaggio() string (messaggio)
+    {
+        Console.WriteLine (messaggio);
+    }
+
+    static void SalvaPartecipanti()
+    {
+        File.WriteAllLines(filePath, partecipanti);
+    }
+    static void InserisciPartecipante()
+    {
+        Console.Write("Nome partecipante: ");
+        string nome = Console.ReadLine();
+        partecipanti.Add(nome);
+        SalvaPartecipanti();
+    }
+    static void VisualizzaPartecipanti()
+    {
+        StampaMessaggio("Elenco partecipanti:");
+        foreach (string partecipante in partecipanti)
+        {
+            StampaMessaggio(partecipante);
+        }
+    }
+    static void OrdinaPartecipanti()
+    {
+        StampaMessaggio("1. Ordine crescente");
+        StampaMessaggio()("2. Ordine decrescente");
+        Console.Write("Scelta: ");
+        int ordine = Convert.ToInt32(Console.ReadLine());
+        if (ordine == 1)
+            {
+                partecipanti.Sort();
+            }
+        else if (ordine == 2)
+        {
+            partecipanti.Sort();
+            partecipanti.Reverse();
+        }
+        else
+        {
+            StampaMessaggio("Scelta non valida");
+        }
+        SalvaPartecipanti();
+    }
+    static void CercaPartecipante()
+    {
+        Console.Write("Nome partecipante: ");
+            string nome = Console.ReadLine();
+            if (partecipanti.Contains(nome))
+            {
+                StampaMessaggio("Il partecipante è presente nella lista");
+            }
+            else
+            {
+                StampaMessaggio("Il partecipante non è presente nella lista");
+            }
+    }
+    static void EliminaPartecipante()
+    {
+        var eliminazione = AnsiConsole.Prompt(
+        new SelectionPrompt<string>()
+            
+            .PageSize(partecipanti.Count)
+            .MoreChoicesText("[grey](Move up and down to reveal more choice)[/]")
+            .AddChoices(partecipanti));
+        
+            if (partecipanti.Contains(eliminazione))
+            {
+                partecipanti.Remove(eliminazione);
+                StampaMessaggio("Il partecipante è stato eliminato dalla lista");
+                SalvaPartecipanti();
+            }
+            else
+            {
+                StampaMessaggio("Il partecipante non è presente nella lista");
+            }
+    }
+    static void ModificaPartecipante()
+    {
+        Console.Write("Nome partecipante: ");
+            string nome = Console.ReadLine();
+            if (partecipanti.Contains(nome))
+            {
+                Console.Write("Nuovo nome: ");
+                string nuovoNome = Console.ReadLine();
+                int indice = partecipanti.IndexOf(nome);
+                partecipanti[indice] = nuovoNome;
+                StampaMessaggio("Il partecipante è stato modificato nella lista");
+                SalvaPartecipanti();
+            }
+            else
+            {
+                Console.WriteLine("Il partecipante non è presente nella lista");
+            }
+    }
+    
+    static void NumeroPartecipanti()
+    {
+        StampaMessaggio($"Numero partecipanti: {partecipanti.Count}");
+    }
+    static void DividiPartecipantiInSquadre()
+    {
+        int split = partecipanti.Count / 2;
+            List<string> squadra1 = partecipanti.GetRange(0, split);
+            List<string> squadra2 = partecipanti.GetRange(split, partecipanti.Count-split);
+            Console.WriteLine("Squadra 1:");
+            foreach (string partecipante in squadra1)
+            {
+                StampaMessaggio(partecipante);
+            }
+            StampaMessaggio("Squadra 2:");
+            foreach (string partecipante in squadra2)
+            {
+                StampaMessaggio(partecipante);
+            }
+    }
+    static void DividiPartecipantiInSquadreRandom()
+    {
+        List<string> squadra1Random = new List<string>();
+        List<string> squadra2Random = new List<string>();
+        List<string> tempPartecipanti = new List<string>(partecipanti);
+        Random random = new Random();
+        while (tempPartecipanti.Count > 0)
+            {
+                int indice = random.Next(tempPartecipanti.Count);
+                string partecipante = tempPartecipanti[indice];
+                tempPartecipanti.RemoveAt(indice);
+                if (squadra1Random.Count < squadra2Random.Count)
+                {
+                    squadra1Random.Add(partecipante);
+                }
+                else
+                {
+                    squadra2Random.Add(partecipante);
+                }
+            }
+            StampaMessaggio("Squadra 1:");
+            foreach (string partecipante in squadra1Random)
+            {
+                StampaMessaggio(partecipante);
+            }
+            Console.WriteLine("Squadra 2:");
+            foreach (string partecipante in squadra2Random)
+            {
+                StampaMessaggio(partecipante);
+            }
+    }
+}
+    

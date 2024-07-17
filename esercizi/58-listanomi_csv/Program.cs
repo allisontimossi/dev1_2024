c169a209947460d641d6f08201ece8b7a5e9d77b
allisontimossi <allison.timossi@hotmail.it>
2024-07-17 10:58:43 +0200

    csv

---
+++ esercizi/58-listanomi_csv/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+string path = @"test.csv";
+
+
+if (!File.Exists(path))
+{
+    File.Create(path).Close();
+}
+
+while (true)
+
+{
+    List<string> nomi = new List<string>(File.ReadAllLines(@"test.csv"));
+    Console.WriteLine("Inserisci nome, cognome ed età");
+    string nome = Console.ReadLine();
+    string cognome = Console.ReadLine();   
+    string eta = Console.ReadLine();
+
+    string anagrafica = ($"{nome}, {cognome}, {eta}");
+
+    if (nomi.Contains(anagrafica))
+    {
+        Console.WriteLine("Nome già presente");
+    }
+    else
+    {
+        File.AppendAllText (path, anagrafica + "\n");
+        Console.WriteLine("Vuoi inserire un altro nome?");
+        string risposta = Console.ReadLine(); 
+        if (risposta == "n")
+        {
+            break;
+        }   
+    }
+}

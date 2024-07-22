60f0bf6f00c479b0086a20e2ad3bd20cbf891c4a
allisontimossi <allison.timossi@hotmail.it>
2024-07-22 15:46:26 +0200

    MasterMind

--- esercizi/60-MasterMind/Program.cs
+++ esercizi/60-MasterMind/Program.cs
@@ -1,12 +1,100 @@
 ﻿using Spectre.Console;
 
-List<string> colours = new List<string>{":yellow_circle:", ":purple_circle:", ":red_circle:", ":green_circle:", ":brown_circle:", ":blue_circle:"};
+int attempts = 10;
+int round = 1;
+string PcCode = "";
+string ourCode = "";
 
-foreach (string dot in colours)
+//creazione della palette colori con emoji
+string giallo = "" + Emoji.Known.YellowCircle;
+string viola = "" + Emoji.Known.PurpleCircle;
+string blu = "" + Emoji.Known.BlueCircle;
+string verde = "" + Emoji.Known.GreenCircle;
+string rosso = "" + Emoji.Known.RedCircle;
+string arancione = "" + Emoji.Known.OrangeCircle;
+string marrone = ""+ Emoji.Known.BrownCircle;
+List<string> palette = new List<string>{giallo, viola, blu, verde, rosso, arancione, marrone};
+
+string [] secretCode = new string [4];
+string [] guessCode = new string [4];   
+
+//generazione del codice segreto
+for (int i = 0; i < secretCode.Length; i++) 
 {
-    Console.WriteLine(dot);
+    Random code = new Random();
+    secretCode [i] = palette[code.Next(0, 7)];
+}
+PcCode = string.Join(" ", secretCode);    
+AnsiConsole.Write(PcCode);
+
+Console.Clear();
+var rule = new Rule("Mastermind Game");
+    rule.LeftJustified();
+    AnsiConsole.Write(rule);
+
+while (attempts > 0)
+{
+    int white = 0;
+    int black = 0;
+
+            for (int j = 0; j < guessCode.Length; j++)
+            {   
+                AnsiConsole.WriteLine("\nScegli il tuo codice: ");
+                guessCode [j] = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                    .PageSize(palette.Count)
+
+                    .AddChoices(palette));
+                    Console.Clear();
+
+                    ourCode = string.Join(" ", guessCode);
+                    AnsiConsole.WriteLine($"\n{PcCode}"); //!!! DA TOGLIERE
+                    AnsiConsole.WriteLine(ourCode);
+            }
+
+    round++;
+    attempts--;
+
+    for (int b = 0; b < 4; b++)                 
+    {
+        if (guessCode[b] == secretCode[b])
+            {
+                black++;
+            }
+        bool contains = guessCode.Contains(secretCode[b]);   
+        if (contains == true && guessCode[b] != secretCode[b])
+            {
+                white++;
+            }
+        if (contains != true && guessCode[b] != secretCode[b])
+            {
+                Console.WriteLine("");
+            }   
+    }
+        //hint
+        Console.Write(""+ Emoji.Known.BlackCircle);
+        Console.WriteLine($": {black}");
+        Console.Write(""+ Emoji.Known.WhiteCircle);
+        Console.Write($": {white}");
+
+    //risultati del round
+    if (ourCode == PcCode)
+    {
+        AnsiConsole.WriteLine("\nHai vinto!");
+        break;
+    }
+    else if (attempts == 0)
+    {
+        AnsiConsole.WriteLine("\nMi dispiace, ma hai perso!");
+        AnsiConsole.WriteLine($"Il codice era {PcCode}");
+    }
+    else if (attempts == 1)
+    {
+        AnsiConsole.WriteLine("\nTic-tac, hai ancora un tentativo.");
+    }
+    else
+    {
+        AnsiConsole.WriteLine($"\nRitenta, hai ancora {attempts} tentativi.");
+    }
 }
 
-string [] code = new string [4];
-
-// codice.Length 
--- esercizi/9999-prove/Program.cs
+++ esercizi/9999-prove/Program.cs
@@ -1,15 +1,21 @@
-﻿﻿using Newtonsoft.Json;
+﻿﻿using Spectre.Console;
 
-string path = @"test.csv"; // In questo caso il file csv è nella stessa cartella del programma 
-string[] lines = File.ReadAllLines(path); // legge tutte le righe del file
-string[][] prodotti = new string[lines.Length][]; // crea un array di array di stringhe con la lunghezza del numero di righe del file
-for(int i = 0; i < lines.Length; i++)
+namespace Panels;
+
+public static class Program
 {
-    prodotti[i] = lines[i].Split(','); // assegna ad ogni elemento dell'array di array di stringhe il valore della riga corrispondente divisa in un array di stringhe ed utilizza la virgola come separatore
-}
-for(int i = 0; i < prodotti.Length; i++)
-{
-    string path2 = prodotti[i+1][0] + ".json"; // Crea i files utilizzando la chiave del csv come nome
-    File.Create(path2).Close();
-    File.AppendAllText(path2, JsonConvert.SerializeObject(new { nome = prodotti[i][0], prezzo = prodotti[i][1] })); // Scrive la riga nel file
+    public static void Main()
+    {
+
+        // Left adjusted panel with text
+        AnsiConsole.Write(
+            new Panel(new Text
+            (
+                
+            ).LeftJustified())
+                .Expand()
+                .SquareBorder()
+                .Header("[red]Turno[/]"));
+    }
+
 }
